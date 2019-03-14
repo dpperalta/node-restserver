2bdf8cdce435db35be7723d3a1280270eec6a2fb
Diego Peralta <die_go_p@hotmail.com>
2019-03-13 19:15:07 -0500

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,21 @@
 // =========================================
 //          PUERTO DE BASE DE DATOS
-//=========================================
+//==========================================
 process.env.PORT = process.env.PORT || 3000;
+
+// =========================================
+//          ENTORNO DE DESPLIEGUE
+//==========================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =========================================
+//          AMBIENTE DE BASE DE DATOS
+//==========================================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://dpperalta:TaAMS626r3UUnbG@cluster0-35acl.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,44 +1,24 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
+// Parser to applicatrion/json -> Para leer en formato JSON los resultados HTML
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
-});
+//Cargar las rutas del usuario
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario y no ha sido ingresado'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true },
+    (err, res) => {
+        if (err) throw err;
+        console.log('Base de Datos ONLINE en el puerto 27017');
     });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-});
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando en el puerto: ', process.env.PORT);
